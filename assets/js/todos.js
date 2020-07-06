8ef9341f4a088d515a21e081713cfc6ff670ae4a
mark-soro <napoleon_soro@rocketmail.com>
2020-07-06 17:14:27 -0400

    added completed logic

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -5,10 +5,20 @@
 //    $(this).css('text-decoration', 'line-through');
 //  }); or you can create an object with all the style properties.
 
+// $('li').click(function(){ // if li is gray, turn it black, else, turn it gray.
+//   if($(this).css('color') === 'rgb(128, 128, 128)'){
+//      $(this).css({
+//        color: 'black',
+//        textDecoration: 'none'
+//        });
+//   } else {
+//     $(this).css({
+//       color: 'gray',
+//       textDecoration: 'line-through' // text-decoration: 'line-through', wrong syntax js does not allow '-'
+//     });
+//   }
+// }); or avoid all of this by using just toggle class
+
 $('li').click(function(){
-  $(this).css({
-    color: 'gray',
-    //text-decoration: 'line-through', wrong syntax js does not allow '-'
-    textDecoration: 'line-through'
-  });
+  $(this).toggleClass('completed');
 });
