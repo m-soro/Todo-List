65756b9408ca64bd5b63dbf66f2890ed229288c1
mark-soro <napoleon_soro@rocketmail.com>
2020-07-07 11:54:21 -0400

    added the toggle feature when header icon is clicked

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -19,12 +19,13 @@
 //   }
 // }); or avoid all of this by using just toggle class like below
 
+// add completed class to the list
 $('ul').on('click','li', (function(){ //on() will add the click event listener to non existing lists
   $(this).toggleClass('completed'); // compare to just click.
   })
 );
 
-
+// remove list items in list
 $('ul').on('click', 'span', (function(event){ // click span to remove item
   $(this).parent().fadeOut(300, function(){ //.parent will remove the span's parent's which is the list.
     $(this).remove();
@@ -33,11 +34,16 @@ $('ul').on('click', 'span', (function(event){ // click span to remove item
   })
 );
 
-
+// add input in the list
 $("input[type='text']").keypress(function(event){
   if(event.which === 13){ // 13 is the key value of enter
     let toDoInput = $(this).val(); // take the input and save it on a variable.
     $(this).val(''); // clear the text area
     $('ul').append('<li><span><i class="fa fa-trash"></i></span> '+ toDoInput +'</li>') // append the to do variable in the list.
   }
+});
+
+// toggle the input box by clicking the clipboad icon
+$('.fa-clipboard-list').click(function(){
+  $("input[type='text']").fadeToggle();
 });
